3833ea373e7742bae6eddfabc475fc963d626e06
刘放 <362512489@qq.com>
2019-07-05 09:34:02 +0800

    refactor: add interview

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const shell = require("shelljs");
 var http = require("http");
 var createHandler = require("github-webhook-handler");
 var handler = createHandler({ path: "/pushCode", secret: "study" }); // 根据git上webhook的配置填写
-// var handlerInterview = createHandler({ path: "/pushCodeInterview", secret: "study" }); // 根据git上webhook的配置填写
+var handlerInterview = createHandler({ path: "/pushCodeInterview", secret: "study" }); // 根据git上webhook的配置填写
 http
     .createServer(function (req, res) {
         handler(req, res, function (err) {
             res.statusCode = 404;
             res.end("no such location");
         });
-        // handlerInterview(req, res, function (err) {
-        //     res.statusCode = 404;
-        //     res.end("no such location");
-        // });
+        handlerInterview(req, res, function (err) {
+            res.statusCode = 404;
+            res.end("no such location");
+        });
     })
     .listen(7777);
 
@@ -32,17 +32,17 @@ handler.on("push", function (event) {
         init();
     }
 });
-// handlerInterview.on("push", function (event) {
-//     console.log(
-//         "Received a push event for %s to %s",
-//         event.payload.repository.name,
-//         event.payload.ref
-//     );
+handlerInterview.on("push", function (event) {
+    console.log(
+        "Received a push event for %s to %s",
+        event.payload.repository.name,
+        event.payload.ref
+    );
 
-//     if (event.payload.ref == "refs/heads/master") {
-//         handlerInterview();
-//     }
-// });
+    if (event.payload.ref == "refs/heads/master") {
+        handlerInterview();
+    }
+});
 
 function init() {
     console.log("init deploy task");
